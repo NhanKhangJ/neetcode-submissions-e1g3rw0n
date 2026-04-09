df5bda76efe0438085506f419bbf50e5c66cc521
NhanKhangJ <90268430+NhanKhangJ@users.noreply.github.com>
2026-04-08 23:34:01 -0600

    Add: baseball-game - submission-3

---
+++ Data Structures & Algorithms/baseball-game/submission-3.cpp
@@ -0,0 +1,32 @@
+#include <stack>
+
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> scores;
+        int result = 0;
+        for(auto const& op : operations){
+            if(op == "+"){
+                int top1 = scores.top(); scores.pop();
+                int top2 = scores.top();
+                int sum = top1 + top2;
+                scores.push(top2);
+                scores.push(top1);
+                scores.push(sum);
+                result += sum;
+            } else if(op == "D"){
+                int doubled = scores.top() * 2;
+                scores.push(doubled);
+                result += doubled;
+            } else if(op == "C"){
+                result -= scores.top();
+                scores.pop();
+            } else {
+                int val = stoi(op);
+                scores.push(val);
+                result += val;
+            }
+        }
+        return result;
+    }
+};
