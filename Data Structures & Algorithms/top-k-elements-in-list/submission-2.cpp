cec6a68bb9ce59dc69d97bad6516b697e724d3cf
NhanKhangJ <90268430+NhanKhangJ@users.noreply.github.com>
2026-05-30 15:07:49 -0600

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // we can create a map of pair data structure
+        unordered_map<int, int> iMap;
+        // loops through vector to create the map;
+        for(int n : nums) {
+            iMap[n]++;
+        }
+        // a vectore of pair instead
+        vector<pair<int,int>> pairList;
+        for(const auto& [key, value] : iMap){
+            pairList.push_back({key, value});
+        }        
+        // perform a sort;
+        // ✅ Should be
+        sort(pairList.begin(), pairList.end(), [](auto& a, auto& b){
+            return a.second > b.second; // descending by frequency
+        });
+        // take the first k after sort;
+        vector<int> result;
+        for(int i = 0; i < k; i++){
+            result.push_back(pairList[i].first);
+        }
+        return result;
+    }
+};
